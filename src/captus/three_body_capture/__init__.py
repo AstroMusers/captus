5fd6b1f23f0c1bcb261737d2559f6e2b6fdc61ae
aesar77 <a.saricaoglu@wustl.edu>
2026-09-15 13:03:42 -0500

    Add Plots to __all__ in three_body_capture module for improved accessibility

--- src/captus/three_body_capture/__init__.py
+++ src/captus/three_body_capture/__init__.py
@@ -1,10 +1,11 @@
 from .configurations import Configuration
 from .simulations import ThreeBodyCapture, PBHPopulation, ThreeBodyEvolution
-
+from .plotting import Plots
 
 __all__ = [
     "Configuration",
     "ThreeBodyCapture",
     "PBHPopulation",
     "ThreeBodyEvolution",
+
 ]
